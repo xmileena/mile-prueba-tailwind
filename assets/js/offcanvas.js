6a34b2bf608363e9519f94a93a6c1bdf97750bcc
xmileena <milena.belen.miranda@gmail.com>
2023-07-09 15:59:29 -0300

    blog grid

--- assets/js/offcanvas.js
+++ assets/js/offcanvas.js
@@ -1,9 +1,17 @@
-document.getElementById('burger-menu').addEventListener('click', function () {
-  var offcanvas = document.getElementById('offcanvas');
-  offcanvas.classList.toggle('open');
+// Mobile menu toggle
+const menuButton = document.querySelector('.lg\\:hidden button');
+const mobileMenu = document.querySelector('.lg\\:hidden[role="dialog"]');
+
+menuButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
 });
 
-document.getElementById('close-button').addEventListener('click', function () {
-  var offcanvas = document.getElementById('offcanvas');
-  offcanvas.classList.remove('open');
-})
+// Close mobile menu when clicking outside
+document.addEventListener('click', (event) => {
+  const targetElement = event.target;
+  const isMenuOpen = !mobileMenu.classList.contains('hidden');
+
+  if (isMenuOpen && !targetElement.closest('.lg\\:hidden')) {
+    mobileMenu.classList.add('hidden');
+  }
+});
